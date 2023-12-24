501069a3ed9d9fbfc624b8eab45b9ea9b1fff488
yonemuramio <151986427+yonemuramio@users.noreply.github.com>
2023-12-23 23:57:58 -0500

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+            $onion_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+            print_r($onion_data);
+
+        ?>
+    </p>
+</body>
+
+</html>
